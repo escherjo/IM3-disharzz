a51bf4d98b1227fd0007df56f33a1327595a5bac
Jonas Escher <j.escher@tincan.ch>
2022-11-12 16:27:51 +0100

    removed auth class from api endpoint

--- api/projects.php
+++ api/projects.php
@@ -1,7 +1,5 @@
 <?php
-require_once '../class/auth.php';
 require_once '../class/projects.php';
-$auth = new Auth();
 $projects = new Projects();
   // get projects and serve as json for javascript to use  
 $projects = $projects->getAllProjects();
