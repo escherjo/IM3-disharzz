e5d79ad49d8c466b274644519de8018664815f70
Jonas Escher <foh@jonasescher.ch>
2023-01-10 14:14:49 +0100

    README Update, init.sql added, fixed fonts in main.css

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # disharzz
+
+Eine Digezzprojekt-Sharing Plattform für das Modul Interaktive Medien 3.
+Schwerpunkt dieses Moduls war Backend Technologien mit PHP.
+
+## Live Beispiel
+
+Ein Live Beispiel findest du [Hier](https://185457-1.web.fhgr.ch/)
+
+
+## Konfiguration
+
+Kopiere die Datei ['db_empty.php'](./class/db_empty) im Verzeichnis ['class'](./class) zu db.php
+Hier werden die MySQL oder MariaDB Credentials angegeben. 
+```javascript
+    private $db_username = 'user'; // Benutzername
+    private $db_password = 'password'; // Passwort
+    private $db = 'host'; // IP der Datenbank
+    private $db_name = 'database'; // Name der Datenbank
+    private $db_port = '3306'; // Port
+```
+
+Auf deiner Datenbank dann den ['Datenbank Dump'](./init.sql) in deiner Datenbank importieren.
+
+
+## Lokale Dev Umgebung
+
+Wenn du Docker installiert hast einfach im Root Verzeichnis in der ['Docker Konfiguration'](./docker-compose.yml) deine Passwort für MariaDB setzen, dieses in deiner db.php einfügen und in der Konsole docker-compose up durchführen. Nun hast du 3 Services gestartet (Apache mit php8 / MariaDB / phpmyadmin)
+
+Mit phpmyadmin eine Datenbank erstellen und den ['Datenbank Dump'](./init.sql) importieren.
+
+## Deploy
+
+1. Auf deinem Server eine Datenbank erstellen
+2. ['Datenbank Dump'](./init.sql) importieren
+3. die db.php im ordner ['class'](./class) entsprechend anpassen
+4. Alles auf deinen Webserver laden.
---
+++ init.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 10, 2023 at 02:02 PM
+-- Server version: 10.3.31-MariaDB-0+deb10u1
+-- PHP Version: 7.0.33-57+0~20211119.61+debian10~1.gbp5d8ba5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `185457_1_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `tags` text NOT NULL,
+  `open` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `name` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
