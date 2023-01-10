4e6912de3e08549e4e78b17dbdbe697170d37987
Jonas Escher <foh@jonasescher.ch>
2023-01-10 13:50:31 +0100

    Page title

--- src/layout/head.php
+++ src/layout/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Test</title>
+    <title>Disharzz</title>
     <link rel="stylesheet" href="/css/main.css">
     <script src="/js/main.js"></script>
 </head>
